4c0572907123685e9bbee58a1fcf46c1104ee693
Alexios Karadimos <karadalex@gmail.com>
2020-12-19 00:00:34 +0200

    Setup new react native app with bottom tabs navigation and Typescript

---
+++ mobile/navigation/BottomTabNavigator.tsx
@@ -0,0 +1,69 @@
+import { Ionicons } from '@expo/vector-icons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+import * as React from 'react';
+import Colors from '../constants/Colors';
+import useColorScheme from '../hooks/useColorScheme';
+import { BottomTabParamList } from '../types';
+
+import Dashboard from '../screens/Dashboard';
+import Prices from '../screens/Prices';
+import Trade from '../screens/Trade';
+import Portfolio from '../screens/Portfolio';
+import Account from '../screens/Account';
+
+
+const BottomTab = createBottomTabNavigator<BottomTabParamList>();
+
+export default function BottomTabNavigator() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <BottomTab.Navigator
+      initialRouteName="Dashboard"
+      tabBarOptions={{ activeTintColor: Colors[colorScheme].tint }}
+    >
+      <BottomTab.Screen
+        name="Dashboard"
+        component={Dashboard}
+        options={{
+          tabBarIcon: ({ color }) => <TabBarIcon name="ios-code" color={color} />,
+        }}
+      />
+      <BottomTab.Screen
+        name="Prices"
+        component={Prices}
+        options={{
+          tabBarIcon: ({ color }) => <TabBarIcon name="ios-code" color={color} />,
+        }}
+      />
+      <BottomTab.Screen
+        name="Trade"
+        component={Trade}
+        options={{
+          tabBarIcon: ({ color }) => <TabBarIcon name="ios-code" color={color} />,
+        }}
+      />
+      <BottomTab.Screen
+        name="Portfolio"
+        component={Portfolio}
+        options={{
+          tabBarIcon: ({ color }) => <TabBarIcon name="ios-code" color={color} />,
+        }}
+      />
+      <BottomTab.Screen
+        name="Account"
+        component={Account}
+        options={{
+          tabBarIcon: ({ color }) => <TabBarIcon name="ios-code" color={color} />,
+        }}
+      />
+    </BottomTab.Navigator>
+  );
+}
+
+// You can explore the built-in icon families and icons on the web at:
+// https://icons.expo.fyi/
+function TabBarIcon(props: { name: string; color: string }) {
+  return <Ionicons size={30} style={{ marginBottom: -3 }} {...props} />;
+}
---
+++ mobile/types.tsx
@@ -0,0 +1,12 @@
+export type RootStackParamList = {
+  Root: undefined;
+  NotFound: undefined;
+};
+
+export type BottomTabParamList = {
+  Dashboard: undefined;
+  Prices: undefined;
+  Trade: undefined;
+  Portfolio: undefined;
+  Account: undefined;
+};
